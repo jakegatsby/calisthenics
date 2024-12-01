5fffea0005fd2e30421e1005a49025014911fd8d
jakegatsby <jakegatsby@example.com>
2024-12-01 08:17:29 -0500

    prehab

--- build.py
+++ build.py
@@ -10,11 +10,14 @@ template = env.get_template("index.html.j2")
 with open("skills.md") as f:
   skills = markdown.markdown(f.read())
 
-
 with open("mobility.md") as f:
   mobility = markdown.markdown(f.read())
 
+with open("prehab_rehab.md") as f:
+  prehab_rehab = markdown.markdown(f.read())
+
+
 
 with open("index.html", "w") as f:
-  f.write(template.render(skills=skills, mobility=mobility))
+  f.write(template.render(skills=skills, mobility=mobility, prehab_rehab=prehab_rehab))
 
---
+++ prehab_rehab.md
@@ -0,0 +1,3 @@
+#### Back Prehab/Rehab
+
+- [Original 12 Minutes of Foundation Training with Dr. Eric Goodman](https://www.youtube.com/watch?v=4BOTvaRaDjI)
